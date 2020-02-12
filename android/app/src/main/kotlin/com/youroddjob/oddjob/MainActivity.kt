d4328de2e8b6b7cfb257cfdd8fd17b14da3806f6
Trevor Okino <trevor.okino@uj.edu>
2020-02-11 20:07:21 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/youroddjob/oddjob/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.youroddjob.oddjob
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}

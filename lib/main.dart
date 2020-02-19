9477b06a45f150d91efd5526dd63b30a03fcffdc
Garret Browning <garret.browning@uj.edu>
2020-02-18 18:09:00 -0600

    This is the initial startings of the Main Menu Page - We have 4 buttons here which all backtrack back to the Main Menu.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart.';
 
 void main() {
   runApp(MaterialApp(
@@ -9,54 +9,119 @@ void main() {
 
 // BUILDING A MULTI-PAGE APPLICATION WITH DART's FLUTTER MOBILE FRAMEWORK (Instructional video, Youtube)
 final ThemeData themeData = ThemeData(
-  canvasColor: Colors.deepOrangeAccent,
+  canvasColor: Colors.white70,
   accentColor: Colors.black,
 );
 
-// Page 1 of App
+// LoginPage of App
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: FlatButton(
+        child: RaisedButton(
           onPressed: () {
-            Navigator.push(context, PageTwo());
+            Navigator.push(context, MainMenu());
           },
-          child: Text("Go to Page Two"),
+          child: Text("Log In"),
         ),
       ),
     );
   }
 }
 
-// Page 2 Class
-class PageTwo extends MaterialPageRoute<Null>{
-  PageTwo() : super(builder: (BuildContext context) {
+// MainMenu Class
+class MainMenu extends MaterialPageRoute<Null> {
+  MainMenu() : super(builder: (BuildContext context) {
      return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).canvasColor,
           elevation: 1.0,
         ),
-        body: Center(
-          child: RaisedButton(
-            onPressed: () {
-              Navigator.push(context, PageThree());
-           },
-           child: Text("Go to Page Three"),
-          ),
-        ),
-     );
+         body: Column(children: <Widget>[
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceAround,
+             children: <Widget>[
+               Text(
+                 "Browse OddJobs",
+                 style: TextStyle(color: Colors.blueAccent, fontSize: 25),
+               ),
+               Text(
+                 "Post An OddJob",
+                 style: TextStyle(color: Colors.orangeAccent, fontSize: 25),
+               ),
+             ],
+           ),
+           Row(
+             //ROW 1
+             mainAxisAlignment: MainAxisAlignment.spaceAround,
+             children: [
+               Container(
+                 color: Colors.orange,
+                 margin: EdgeInsets.all(25.0),
+                 child: RaisedButton(
+                   onPressed: () {
+                     Navigator.push(context, BrowseJobPage());
+                   },
+                 ),
+               ),
+               Container(
+                 color: Colors.blue,
+                 margin: EdgeInsets.all(25.0),
+                 child: RaisedButton(
+                   onPressed: () {
+                     Navigator.push(context, PostJobPage());
+                   },
+                 ),
+               ),
+             ],
+           ),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceAround,
+             children: <Widget>[
+               Text(
+                 "Manage Your Account",
+                 style: TextStyle(color: Colors.purpleAccent, fontSize: 25),
+               ),
+               Text(
+                 "Help/Support",
+                 style: TextStyle(color: Colors.greenAccent, fontSize: 25),
+               ),
+             ],
+           ),
+           Row( //ROW 2
+               mainAxisAlignment: MainAxisAlignment.spaceAround,
+               children: [
+                 Container(
+                   color: Colors.purple,
+                   margin: EdgeInsets.all(25.0),
+                   child: RaisedButton(
+                     onPressed: () {
+                       Navigator.push(context, ManageAccountPage());
+                     },
+                   ),
+                 ),
+                 Container(
+                   color: Colors.green,
+                   margin: EdgeInsets.all(25.0),
+                   child: RaisedButton(
+                     onPressed: () {
+                       Navigator.push(context, HelpSupportPage());
+                     },
+                   ),
+                 ),
+               ]),
+         ]));
   });
 }
 
-// Page 3 Class
-class PageThree extends MaterialPageRoute<Null> {
-  PageThree() : super(builder: (BuildContext context) {
+// BrowseJobPage Class
+class BrowseJobPage extends MaterialPageRoute<Null> {
+  BrowseJobPage() : super(builder: (BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Last Page!"),
+        title: Text("Browse OddJobs"),
         backgroundColor: Theme.of(context).accentColor,
         elevation: 2.0,
         actions: <Widget>[
@@ -71,10 +136,101 @@ class PageThree extends MaterialPageRoute<Null> {
       body: Center(
         child: MaterialButton(
           onPressed: (){
-            Navigator.popUntil(context, ModalRoute.withName(Navigator.defaultRouteName));
+            Navigator.push(context, MainMenu());
           },
-          child: Text("Go Home!"),
+          child: Text("Go Home! (From Browsing OddJobs)"),
         )
+      ),
+    );
+  });
+}
+// PostJobPage Class
+class PostJobPage extends MaterialPageRoute<Null> {
+  PostJobPage() : super(builder: (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Post An OddJob!"),
+        backgroundColor: Theme
+            .of(context)
+            .accentColor,
+        elevation: 2.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+      body: Center(
+          child: MaterialButton(
+            onPressed: () {
+              Navigator.push(context, MainMenu());
+            },
+            child: Text("Go Home! (From Posting An OddJob)"),
+          )
+      ),
+    );
+  });
+}
+// ManageAccountPage Class
+class ManageAccountPage extends MaterialPageRoute<Null> {
+  ManageAccountPage() : super(builder: (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Manage Your Account"),
+        backgroundColor: Theme
+            .of(context)
+            .accentColor,
+        elevation: 2.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+      body: Center(
+          child: MaterialButton(
+            onPressed: () {
+              Navigator.push(context, MainMenu());
+            },
+            child: Text("Go Home! (From Managing Your Account)"),
+          )
+      ),
+    );
+  });
+}
+
+// HelpSupportPage Class
+class HelpSupportPage extends MaterialPageRoute<Null> {
+  HelpSupportPage() : super(builder: (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Help/Support!"),
+        backgroundColor: Theme
+            .of(context)
+            .accentColor,
+        elevation: 2.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+      body: Center(
+          child: MaterialButton(
+            onPressed: () {
+              Navigator.push(context, MainMenu());
+            },
+            child: Text("Go Home! (From Help/Support)"),
+          )
       ),
     );
   });
